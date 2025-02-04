6105e27eea2e8476c2e417cb6d67616511c436e9
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-02-04 16:44:46 +0300

    Add project files.

---
+++ Core/Movie.Domain/Entities/Review.cs
@@ -0,0 +1,11 @@
+﻿namespace Movie.Domain.Entities
+{
+    public class Review
+    {
+        public int ReviewID { get; set; }
+        public string? Comment { get; set; }
+        public decimal Rating { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow; // Yorum tarihi
+        public bool IsApproved { get; set; } = false; // Yorum onaylandı mı?
+    }
+}
