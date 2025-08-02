4775c4828832dd18290284699dc2bf6d2c8c2a72
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-08-02 18:52:59 +0300

    Api Register İşlemi ve UI Teması

---
+++ Frontend/Movie.UI/Controllers/RegisterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movie.DTO.DTOs.UserRegisterDTOs;
+
+namespace Movie.UI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(CreateUserRegisterDTO createUserRegisterDTO)
+        {
+            return RedirectToAction("SignIn", "Login");
+        }
+    }
+}
