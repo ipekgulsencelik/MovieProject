be4dfdc61c3e14b6a6bd912ee4c03611a7c440c5
ipekgulsencelik <ipekgulsen.celik@gmail.com>
2025-12-20 01:57:34 +0300

    Login Sayfası Tasarımı Oluşturma

---
+++ Frontend/Movie.UI/Controllers/LoginController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Movie.UI.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
